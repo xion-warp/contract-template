c0e92f488419af8a5a9f8c3b7b9eb70fbf6810db
Reyth3 <mrreynevan2@gmail.com>
2024-02-08 00:12:04 +0100

    fix: files

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use shared::<CONTRACT_NAME>::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
