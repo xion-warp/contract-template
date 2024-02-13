4adf3d979c9f0ed5e2425310a151c426b88e69da
Reyth3 <mrreynevan2@gmail.com>
2024-02-13 23:03:49 +0100

    fix: missing and broken code

--- src/contract.rs
+++ src/contract.rs
@@ -1,12 +1,11 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 
-use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult,
-};
+use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
+use crate::error::ContractError;
 use crate::state::Config;
-use shared::<CONTRACT_NAME>::msg::{ExecuteMsg, InstantiateMsg, OwnerResponse, QueryMsg, MigrateMsg};
+use shared::contr::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, OwnerResponse, QueryMsg};
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
@@ -15,7 +14,7 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
     let config = Config {
-        owner: deps.api.addr_canonicalize(info.sender.as_str())?,
+        owner: deps.api.addr_validate(_info.sender.as_str())?,
         message: "".to_owned(),
     };
 
@@ -31,24 +30,17 @@ pub fn execute(
     info: MessageInfo,
     // Note: implement this function with different type to add support for custom messages
     // and then import the rest of this contract code.
-    msg: ExecuteMsg<Empty>,
-) -> Result<Response<Empty>, ContractError> {
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
     match msg {
         ExecuteMsg::ChangeOwner { addr } => todo!(),
     }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    // Note: implement this function with different type to add support for custom messages
-    // and then import the rest of this contract code.
-    msg: ExecuteMsg<Empty>,
-) -> Result<Response<Empty>, ContractError> {
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetOwner {} => to_binary(&query_owner(deps)?),
+        QueryMsg::GetOwner {} => to_json_binary(&query_owner(deps)?),
     }
 }
 
